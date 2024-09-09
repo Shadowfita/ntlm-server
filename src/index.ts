fb0a2b3ab032f2e8aa58ef78090af4811ac848bb
Ryan Palmer <admin@ryanpalmer.tech>
2024-09-09 13:31:00 +1000

    Case amendment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export * from "./constants";
 
-export { default as NtlmNegotiationMessage } from "./messages/ntlm_negotiation_message";
-export { default as NtlmChallengeMessage } from "./messages/ntlm_challenge_message";
-export { default as NtlmAuthenticateMessage } from "./messages/ntlm_authenticate_message";
+export { default as NTLMNegotiationMessage } from "./messages/ntlm_negotiation_message";
+export { default as NTLMChallengeMessage } from "./messages/ntlm_challenge_message";
+export { default as NTLMAuthenticateMessage } from "./messages/ntlm_authenticate_message";
 
 export { default as MessageFields } from "./messages/fields/message_fields";
 export { default as NegotiationFlags } from "./messages/fields/negotiation_flags";
