0c1fcc332fb5212ca9a084ea597dbace6c67e111
Ryan Palmer <admin@ryanpalmer.tech>
2024-09-09 13:27:13 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./constants";
-export * from "./messages/ntlm_negotiation_message";
-export * from "./messages/ntlm_challenge_message";
-export * from "./messages/ntlm_authenticate_message";
-export * from "./messages/fields/message_fields";
-export * from "./messages/fields/negotiation_flags";
+
+export { default as NtlmNegotiationMessage } from "./messages/ntlm_negotiation_message";
+export { default as NtlmChallengeMessage } from "./messages/ntlm_challenge_message";
+export { default as NtlmAuthenticateMessage } from "./messages/ntlm_authenticate_message";
+
+export { default as MessageFields } from "./messages/fields/message_fields";
+export { default as NegotiationFlags } from "./messages/fields/negotiation_flags";
